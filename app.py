e2b4ad1713a3d1fbf9df4ddb967b14e1ed012235
wemark <we.mark@web.de>
2014-11-14 00:39:46 +0100

    added error handling

--- app.py
+++ app.py
@@ -6,13 +6,29 @@ from flask import Flask
 #create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
 
 # define the view using a function, which returns a string
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!?!?"
+
+# dynamic route
+@app.route("/test/<value>")
+def search(value):
+    return value
+
+# dynamic rout with explicit status codes
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
 
 # start the development server using the run() method
 if __name__ == "__main__":
